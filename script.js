760816512c2808aa73da75ce1da0ac782bdde963
Alexandre LOUBEAU <loubeau.alexandre3@gmail.com>
2022-11-03 20:55:07 +0100

    remaniement du CSS avec les ajouts des champs JS du formulaire. Ajout event listener. Diverses anomalies sur la code JS (partie formulaire adresse). A revoir plus tard.

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+const divID = document.getElementById('city')
+const selector = document.getElementById('select-city')
+const selectLocate = document.querySelector('select')
+let result = document.querySelector('option')
+result = selector.value
+let selectValues;
+divID.style.display = 'none'
+const divStreetID = document.getElementById('street')
+divStreetID.style.display = 'none'
+
+let queryLocate
+
+let adress = document.getElementById('adress')
+
+let cityCode = ""
+
 function btn_postalCodeGET() {
-  let queryLocate = document.getElementById('postalCode').value
-  const url = "https://api-adresse.data.gouv.fr/search/?q=postcode=" + queryLocate
+  selectLocate.innerHTML=""
+  queryLocate = document.getElementById('postalCode').value
+  const url = "https://api-adresse.data.gouv.fr/search/?q=postcode=" + queryLocate + "&type=municipality"
   $.get(url, postalCode).done(function() {
     //alert( "second success" );
   })
     .fail(function() {
-      alert("error");
+      alert("erreur lors de la requête");
     })
     .always(function() {
       //alert( "finished" );
@@ -13,59 +30,63 @@ function btn_postalCodeGET() {
 }
 
 const postalCode = function(data) {
-  let sectionID = document.getElementById('city')
-  console.log("données API ", data)
-  // TEST DE L'API//
-  let donnee = data.features[4].properties.city
-  console.log(donnee)
-  // FIN TEST //
-
-  
-  console.log(sectionID) //<= Regarde ce que donne ça ;), Alex : Merci Jérémy //
-
-  //Construction du select HTML //
-  sectionID.innerHTML = `<label for="select-city">Choisissez une ville : </label>
-  <select name="city" id="select-city" onclick="select_cityGET()">
-    <option value="">Choisissez une ville</option>`
-  selectID = document.getElementById('select-city')
-  //fin construction SELECT HTML //
-
-  // Construction des options de la ville //
-    // test constructeur tableau pour condition if//
-    let City = []
-  data.features.forEach((e,i) => {
-    console.log(City) // console.log pour donnée tableau
-    console.log(data.features[i].properties.city)
-    // fin test//
-    if(!City.includes(data.features[i].properties.city)) {
-    City.push(data.features[i].properties.city)
-    selectID.innerHTML += `<option class="city" id="` + i + `" value="`+ data.features[i].properties.city + `">` + data.features[i].properties.city +"</option>"
-    } else {
-      
-    }
+  console.log(data)
+  console.log(divID) //<= Regarde ce que donne ça ;), Alex : Merci Jérémy //
+  divID.style.display = ''
+  data.features.forEach(e => {
+    selector.innerHTML += `<option class="city" value="` + e.properties.citycode + `" id="` +  e.properties.city + `">` + e.properties.city + "</option>"
   })
-  // Fin construction des options villes //
 
 }
 
 
 // Construction du bouton onclick VILLE //
 function select_cityGET() {
-  let selectLocate = document.querySelector('select')
   console.log(selectLocate)
 
   // TEST //
   selectLocate.addEventListener('change', (event) => {
-    const result = document.querySelector('option')
-
     const paraID = document.getElementById('cityResult')
-    let test = result.innerHTML = `You like ${event.target.value}`;
-    console.log(test)
-  console.log(paraID)
-    paraID.innerHTML = result.innerHTML
+    result = `${event.target.value}`
+    divStreetID.style.display = ''
+    console.log(result)
+    cityCode = result
+    paraID.setAttribute("id",`${event.target.value}`)
+    console.log(cityCode)
+    paraID.innerHTML = result
+    
     //fin test//
   });
-                                
+}
+
+function select_adresseGET() {
+  adress = adress.value
+  adress = adress.trim()
+  adress = adress.split(" ")
+  adress = adress.join("+")
+  console.log(adress)
+  queryLocate = document.getElementById('postalCode').value
+  const url = "https://api-adresse.data.gouv.fr/search/?q=" + adress +"&citycode=" + cityCode + "&city=" + selector.value
+  console.log(url)
+    $.get(url, adressMatch).done(function() {
+    //alert( "second success" );
+  })
+    .fail(function() {
+      alert("erreur lors de la requête");
+    })
+    .always(function() {
+      //alert( "finished" );
+    });
+}
+
+const adressMatch = function adressMatch(data) {
+  console.log(data)
+  data.features.forEach(e => {
+    divStreetID.innerHTML += `<div class="block"> <button>` + e.properties.name + `</button></div>`
+  }) // test du ForEACH
+  console.log(adress)
+}
+  // TESTER UN INPUT ONCHANGE
   // let queryLocateCity = queryLocate.querySelector('main > div > p > button').value //
   //console.log(queryLocateCity)  CONSOLE LOG QUE LEVES (Code postal : 28 300)
 
@@ -87,4 +108,3 @@ function select_cityGET() {
     .always(function() {
       //alert( "finished" );
     }); */
-} 
